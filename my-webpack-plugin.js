7f60433dec07893259a8c859b81f2d7c33c7c3e2
sangwook <bnbt3@naver.com>
2024-03-11 18:28:32 +0900

    웹팩 : 플러그인

---
+++ my-webpack-plugin.js
@@ -0,0 +1,18 @@
+class MyPlugin {
+    apply(compiler) {
+        compiler.hooks.done.tap("My Plugin", (stats) => {
+            console.log("MyPlugin worked");
+        });
+
+        compiler.hooks.emit.tapAsync("My Plugin", (compilation, callback) => {
+            console.log(
+                "compilation.assets['main.js']",
+                compilation.assets["main.js"].source()
+            );
+
+            callback();
+        });
+    }
+}
+
+module.exports = MyPlugin;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
@@ -7,7 +11,7 @@ module.exports = {
     },
     output: {
         filename: "[name].js",
-        path: path.resolve("./dist"),
+        path: __dirname + "/dist",
         assetModuleFilename: "[name][ext]?[hash]"
     },
     module: {
@@ -21,5 +25,31 @@ module.exports = {
                 type: "asset"
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: `
+                Build Date: ${new Date().toLocaleString()}
+                Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+            `
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            templateParameters: {
+                env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+            },
+            minify:
+                process.env.NODE_ENV === "production"
+                    ? {
+                        collapseWhitespace: true, // 빈칸 제거
+                        removeComments: true // 주석 제거
+                    }
+                    : false,
+            hash: process.env.NODE_ENV === "production"
+        }),
+        new CleanWebpackPlugin(),
+        ...(process.env.NODE_ENV === "production"
+            ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+            : [])
+    ]
 };
